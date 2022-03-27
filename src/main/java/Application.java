5020dda2f6439f213b36f627c4d5f1667ba063a0
krasnovsasha <pancasancha4@gmail.com>
2022-03-27 19:02:02 +0300

    feat: counter for output messages

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,5 +3,6 @@ import com.tcs.edu.printer.ConsolePrinter;
 class Application {
     public static void main(String[] args) {
         ConsolePrinter.print("Hello world!");
+        ConsolePrinter.print("Hello world!");
     }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -3,19 +3,21 @@ package com.tcs.edu.decorator;
 import java.util.Date;
 
 /**
- * The {@code TimestampMessageDecorator} class contains method for change
+ * The {@code TimestampMessageDecorator} class contains method {@code decorate} for change
  * base message output.
  */
 public class TimestampMessageDecorator {
+    private static int messageCount;
     /**
      * @param message incoming base message
-     * @return  message with changes
+     * @return decoratedMessage with changes
      * <p>
      * @see com.tcs.edu.printer.ConsolePrinter#print(String)
      */
     public static String decorate(String message){
         String decoratedMessage;
-        decoratedMessage = new Date() + " " + message;
+        messageCount++;
+        decoratedMessage = messageCount + " " + new Date() + " " + message;
         return decoratedMessage;
     }
 }
