42c3ffff1d85bdf444230db977fefece6bea1272
krasnovsasha <pancasancha4@gmail.com>
2022-03-27 18:46:44 +0300

    feat: output message has timestamped

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,5 @@ import com.tcs.edu.printer.ConsolePrinter;
 class Application {
     public static void main(String[] args) {
         ConsolePrinter.print("Hello world!");
-        ConsolePrinter.print("Hello world!");
     }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -1,29 +1,21 @@
 package com.tcs.edu.decorator;
 
-import java.time.Instant;
+import java.util.Date;
 
 /**
- * @author a.a.krasnov
- * The {@code TimestampMessageDecorator} class contains method {@code decorate} for change
+ * The {@code TimestampMessageDecorator} class contains method for change
  * base message output.
  */
 public class TimestampMessageDecorator {
-    private static int messageCount;
-    private static final int PAGE_SIZE = 2;
-
     /**
      * @param message incoming base message
-     * @return new message with changes
-     * <p>
-     * side-effect method changes console output
+     * @return  message with changes
      * <p>
      * @see com.tcs.edu.printer.ConsolePrinter#print(String)
      */
-    public static String decorate(String message) {
-        String decoratedMessage = String.format("%d %s %s", ++messageCount, Instant.now(), message);
-        if (messageCount % PAGE_SIZE == 0) {
-            decoratedMessage = String.format("%s%n---", decoratedMessage);
-        }
+    public static String decorate(String message){
+        String decoratedMessage;
+        decoratedMessage = new Date() + " " + message;
         return decoratedMessage;
     }
 }
