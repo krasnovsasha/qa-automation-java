8780ac35aa46a0ca218c756137b434eda400cdd5
Краснов Александр <a.a.krasnov@tinkoff.ru>
2022-04-24 13:58:28 +0300

    feat: output of missing message

--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -3,6 +3,8 @@ package com.tcs.edu.service;
 import com.tcs.edu.enums.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
 
+import java.util.ArrayList;
+
 import static com.tcs.edu.decorator.SeverityDecorator.decorateSeverityLevel;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorateTimeStamp;
 import static com.tcs.edu.decorator.PageDecorator.*;
@@ -14,15 +16,23 @@ import static com.tcs.edu.decorator.CountDecorator.decorateCount;
 public class MessageService {
     /**
      * API
-     *
-     * @param level   incoming severity
+     *<p>
+     * @param level    incoming severity
      * @param messages incoming messages
      */
-    public static void processMessage(Severity level, String... messages) {
-
-//      It is possible to change decorators positions or turn any of them off for more agile output
+    public static void processMessage(Severity level, String message, String... messages) {
+        ArrayList<String> messagesIncome = new ArrayList<>();
+        if (message != null) {
+            messagesIncome.add(message);
+        }
         for (String s : messages) {
+            if (s != null) {
+                messagesIncome.add(s);
+            }
+        }
+        for (String s : messagesIncome) {
             ConsolePrinter.print(
+//      It is possible to change decorators positions or turn any of them off for more agile output
                     decorateCount() +
                     decorateTimeStamp(s) +
                     decorateSeverityLevel(level) +
--- src/test/Application.java
+++ src/test/Application.java
@@ -3,8 +3,12 @@ import static com.tcs.edu.service.MessageService.processMessage;
 
 class Application {
     public static void main(String[] args) {
-        processMessage(MAJOR,"warning1","warning2","warning3","warning4","warning5","warning6");
-        processMessage(REGULAR,"regular message1","regular message2","regular message3");
-        processMessage(MINOR,"minor message1","minor message2","minor message3","minor message4");
+        //test if 1st param IS NULL
+        processMessage(MAJOR,null,"warning2","warning3","warning4","warning5","warning6");
+        //test if 2nd param IS NULL
+        processMessage(REGULAR,"regular message1",null,"regular message3");
+        //test if all incoming params are NULL
+        processMessage(MINOR,null,null,null);
+
     }
 }
