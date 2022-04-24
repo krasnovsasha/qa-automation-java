adf094139c4d9cd0fc5372348d48c0ea26bf346d
Краснов Александр <a.a.krasnov@tinkoff.ru>
2022-04-24 15:44:45 +0300

    feat: ASC and DESC for incoming messages

---
+++ src/main/java/com/tcs/edu/enums/MessageOrder.java
@@ -0,0 +1,8 @@
+package com.tcs.edu.enums;
+
+/**
+ * @author a.a.krasnov
+ */
+public enum MessageOrder {
+    ASC,DESC;
+}
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.tcs.edu.service;
 
+import com.tcs.edu.enums.MessageOrder;
 import com.tcs.edu.enums.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
 
@@ -16,28 +17,60 @@ import static com.tcs.edu.decorator.CountDecorator.decorateCount;
 public class MessageService {
     /**
      * API
-     *<p>
+     * <p>
      * @param level    incoming severity
      * @param messages incoming messages
+     * <p>
+     * In method ConsolePrinter.print()
+     * It is possible to change decorators positions or turn any of them off for more agile output
      */
     public static void processMessage(Severity level, String message, String... messages) {
-        ArrayList<String> messagesIncome = new ArrayList<>();
-        if (message != null) {
-            messagesIncome.add(message);
-        }
-        for (String s : messages) {
-            if (s != null) {
-                messagesIncome.add(s);
-            }
-        }
+        ArrayList<String> messagesIncome = incomeMessageGenerate(message, messages);
         for (String s : messagesIncome) {
             ConsolePrinter.print(
-//      It is possible to change decorators positions or turn any of them off for more agile output
                     decorateCount() +
                     decorateTimeStamp(s) +
                     decorateSeverityLevel(level) +
                     separateByPage()
             );
         }
+    }
+
+    public static void processMessage(Severity level, MessageOrder order, String message, String... messages) {
+        ArrayList<String> messagesIncome = incomeMessageGenerate(message, messages);
+        if (order.equals(MessageOrder.DESC)) {
+            for (int i = messagesIncome.size() - 1; i >= 0; i--) {
+                ConsolePrinter.print(
+                        decorateCount() +
+                        decorateTimeStamp(messagesIncome.get(i) + " " + (i + 1)) +
+                        decorateSeverityLevel(level) +
+                        separateByPage()
+                );
+            }
+        } else if (order.equals(MessageOrder.ASC)) {
+            for (int i = 0; i <= messagesIncome.size() - 1; i++) {
+                ConsolePrinter.print(
+                        decorateCount() +
+                        decorateTimeStamp(messagesIncome.get(i) + " " + (i + 1)) +
+                        decorateSeverityLevel(level) +
+                        separateByPage()
+                );
+            }
+        }
+    }
+
+    private static ArrayList<String> incomeMessageGenerate(String message, String... messages) {
+        ArrayList<String> messagesIncome = new ArrayList<>();
+        if (message != null) {
+            messagesIncome.add(message);
+        }
+        if (messages != null) {
+            for (String s : messages) {
+                if (s != null) {
+                    messagesIncome.add(s);
+                }
+            }
+        }
+        return messagesIncome;
     }
 }
