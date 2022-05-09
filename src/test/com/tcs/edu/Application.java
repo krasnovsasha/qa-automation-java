e2a01af438c01496749a0f2796ddab23473f5667
Краснов Александр <a.a.krasnov@tinkoff.ru>
2022-05-09 21:13:42 +0300

    refactor: update MessageService API's

--- src/test/com/tcs/edu/Application.java
+++ src/test/com/tcs/edu/Application.java
@@ -1,5 +1,7 @@
 package com.tcs.edu;
 
+import com.tcs.edu.domain.Message;
+
 import static com.tcs.edu.enums.Severity.*;
 import static com.tcs.edu.enums.OutputOrder.*;
 import static com.tcs.edu.enums.Doubling.*;
@@ -7,19 +9,15 @@ import static com.tcs.edu.service.MessageService.log;
 
 class Application {
     public static void main(String[] args) {
-        //test if 1st param IS NULL
-        log(MAJOR, (String) null, "warning2", "warning3", "warning4", "warning5", "warning6");
-        //test if 2nd param IS NULL
-        log(REGULAR, "regular message1", null, "regular message3");
-        //test if all incoming params are NULL
-        log(MINOR, (String) null, (String) null);
-        //test for DESC
-        log(MAJOR,DESC, "message1", "message2", "message3");
-        //test for ASC
-        log(MAJOR,ASC, "message1", "message2", "message3");
-        //test for DISTINCT messages
-        log(MAJOR, DESC, DISTINCT,"messageTheSame","messageTheSame","messageNotTheSame");
-        //test for DOUBLING messages
-        log(MAJOR, ASC, DOUBLES,"messageTheSame","messageTheSame","messageNotTheSame");
+        //test for one message
+        log(new Message(MINOR, "this message has minor priority"));
+        //test for ascending messages
+        log(ASC, new Message(REGULAR, "message first"), new Message(REGULAR, "message second"));
+        //test for descending messages
+        log(DESC, new Message(REGULAR, "message first"), new Message(REGULAR, "message second"));
+        //test for distinct-only messages
+        log(ASC, DISTINCT, new Message(MAJOR, "message1"), new Message(MAJOR, "message1"), new Message(REGULAR, "message3"));
+        //test for messages which may have doubles
+        log(DESC, DOUBLES, new Message(MAJOR, "message1"), new Message(MAJOR, "message1"), new Message(REGULAR, "message3"));
     }
 }
