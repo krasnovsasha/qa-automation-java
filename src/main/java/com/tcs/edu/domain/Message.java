3fab378e3c531c6b09c4a2c2e80d02862f203b71
Краснов Александр <a.a.krasnov@tinkoff.ru>
2022-05-09 21:16:25 +0300

    perf: Message objects compares via override equals() and hashCode()

--- src/main/java/com/tcs/edu/domain/Message.java
+++ src/main/java/com/tcs/edu/domain/Message.java
@@ -2,6 +2,8 @@ package com.tcs.edu.domain;
 
 import com.tcs.edu.enums.Severity;
 
+import java.util.Objects;
+
 /**
  * @author a.a.krasnov
  * <p>
@@ -19,11 +21,31 @@ public class Message {
         this.body = body;
     }
 
+    public Message(String body) {
+        this.body = body;
+    }
+
     public Severity getLevel() {
         return level;
     }
 
     public String getBody() {
         return body;
+    }
+
+    /**
+     * if messages has the same severity and the same body they are the same
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return level == message.level && body.equals(message.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(level, body);
     }
 }
